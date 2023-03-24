88bb388714fe928313aeca59a5353fb5c50857e8
jthuku <jthuku490@gmail.com>
2023-03-24 10:10:42 -0700

    updated 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,9 @@
-# mainfest to install flask
+# Using Puppet, install flask from pip3.
+# Requirements:
+#     Install flask
+#     Version must be 2.1.0
 
 package { 'flask':
-    ensure => '2.1.0',
-    provider => pip3
+  ensure   => '2.1.0',
+  provider => pip3
 }
