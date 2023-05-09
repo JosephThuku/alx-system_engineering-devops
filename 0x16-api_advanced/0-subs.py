a45de7758baba86d549890425ccc37a9c834a4d5
JosephThuku <jthuku490@gmail.com>
2023-05-09 17:14:08 +0000

    fixed some typo

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
-""" Script to list How many subs? """
+"""
+Script to list all subredditts
+"""
 import requests
 
-def number_of_subscribers(subreddit):
-    """ funtion to send request """
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
 
-    if response.status_code == 200:
-        data = response.json()["data"]
-        return data["subscribers"]
-    else:
+def number_of_subscribers(subreddit):
+    """
+    Return number of subscribers from given subreddit
+    Return 0 if subreddit does not exist
+    """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        'User-Agent': 'My Holbie User Agent 1.0',
+    }
+
+    response = requests.get(url, headers=headers).json()
+    if response.get("message") == "Not Found":
         return 0
+    else:
+        return response.get("data").get("subscribers")
--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
 """
-script to returns a list containing the titles 
-of all hot articles for a given subreddit
+recursive function that queries the Reddit API and returns a list
+containing the titles of all hot articles for a given subreddit.
+If no results are found for the given subreddit, the function
+should return None.
 """
 import requests
 
 
 def recurse(subreddit, hot_list=[], after="temp"):
-    """ ecursive function that return titles"""
+    """
+    Recursive function that return titles of all hot articles
+    of a given subreddit and None if no results found
+    """
     url = "https://api.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {
         'User-Agent': 'My Holbie User Agent 1.0',
