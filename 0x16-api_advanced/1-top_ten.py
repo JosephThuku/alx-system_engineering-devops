ea722e1158f4d996aefa9ba7fe43da7cefa103b8
JosephThuku <jthuku490@gmail.com>
2023-05-09 17:09:41 +0000

    made task 1 pep 8 compatible

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python3
 """
-script to prints the titles of the first 10 hot posts
- listed for a given subreddit.
+script to prints the titles of the first
+10 hot posts listed for
+a given subreddit
 """
 import requests
 
+
 def top_ten(subreddit):
-    """ count from afterwards """
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {"User-Agent": "My Holbie User Agent 1.0"}
+    """
+    Return top 10 hot posts from given subreddit
+    """
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    headers = {
+        'User-Agent': 'My Holbie User Agent 1.0',
+    }
 
     response = requests.get(url, headers=headers).json()
-    children_list = response.get("data", {}).get("children", [])
-
-    if not children_list:
+    if response.get("message") == "Not Found":
         print("None")
     else:
-        for child in children_list:
-            print(child["data"]["title"])
-
+        children_list = response.get("data").get("children", [])
+        for titles in children_list:
+            print(titles.get('data').get('title'))
